95ce55dae69cf35dc2217331ee5a2aca223b90b8
Senagomesdev <Senagomes.dev@gmail.com>
2025-01-07 19:39:22 -0300

    Correção hero

--- moove.js
+++ moove.js
@@ -1,25 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('header');
-
-    // Evento de rolagem para ocultar/exibir o menu
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            header.classList.add('scrolled');
-        } else {
-            header.classList.remove('scrolled');
-        }
-    });
-})
-
 const heroBg = document.querySelector('.hero');
 const images = ['img/bg.jpg', 'img/bg-light.jpg']; // Array com as imagens
 let currentIndex = 0;
 
-// Pré-carrega as imagens
-function preloadImages(imageArray) {
+// Função para pré-carregar as imagens
+function preloadImages(imageArray, callback) {
+    let loadedImages = 0;
+    const totalImages = imageArray.length;
+
     imageArray.forEach((src) => {
         const img = new Image();
         img.src = src;
+        img.onload = () => {
+            loadedImages++;
+            if (loadedImages === totalImages) {
+                callback(); // Chama a função após todas as imagens carregarem
+            }
+        };
     });
 }
 
@@ -32,8 +28,8 @@ function changeBackground() {
 // Adiciona uma transição suave para a propriedade backgroundImage
 heroBg.style.transition = 'background-image 1s ease-in-out';
 
-// Pré-carrega as imagens antes de iniciar a troca
-preloadImages(images);
-
-// Configura o intervalo para mudar o fundo automaticamente
-setInterval(changeBackground, 1000); // Troca a cada 3 segundos
+// Inicia o pré-carregamento das imagens e começa a animação após o carregamento
+preloadImages(images, () => {
+    // Troca o fundo automaticamente após todas as imagens serem carregadas
+    setInterval(changeBackground, 1500); // Troca a cada 3 segundos
+});
